8eb5dc02bab3b303152edd2a77b6046206472cd9
Bartek <shox12.47@gmail.com>
2020-08-13 11:52:45 +0200

    Updates

---
+++ precache-manifest.c839a95db8ac517d4e79df7fd0e92ba8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe3d7a7f319c0a92938b7f3130a1db3",
+    "url": "/ZONE-PLN/index.html"
+  },
+  {
+    "revision": "6dd1eab27d99f120ca3b",
+    "url": "/ZONE-PLN/static/js/2.03aac7bf.chunk.js"
+  },
+  {
+    "revision": "c70aa378b0b7d0487bac6cfa60349baf",
+    "url": "/ZONE-PLN/static/js/2.03aac7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2803f931f16fda5347",
+    "url": "/ZONE-PLN/static/js/main.962c216f.chunk.js"
+  },
+  {
+    "revision": "3b672ac740431c1946e3",
+    "url": "/ZONE-PLN/static/js/runtime-main.f629cfc8.js"
+  }
+]);
